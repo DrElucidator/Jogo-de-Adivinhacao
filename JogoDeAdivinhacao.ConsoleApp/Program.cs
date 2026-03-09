3957726f33bf1f592efb91e6dac5a58afaa3977c
Alec Luí <dr.elucidator@gmail.com>
2026-03-09 18:10:19 -0300

    first commit

---
+++ JogoDeAdivinhacao.ConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography;
+bool jogarNovamente = true;
+
+Console.WriteLine("---------------------------------");
+Console.WriteLine("Bem-vindo ao Jogo de Adivinhação!");
+Console.WriteLine("---------------------------------");
+
+while (jogarNovamente == true)
+{
+    int sortNum = RandomNumberGenerator.GetInt32(1, 21);
+    Console.WriteLine("Adivinhe o número entre 1 e 20: ");
+    string? palpite = Console.ReadLine();
+
+    int NumPalpite = Convert.ToInt32(palpite);
+
+    Console.WriteLine("O número digitado foi: " + palpite);
+    Console.WriteLine("O número sorteado foi: " + sortNum);
+
+    if (NumPalpite == sortNum)
+    {
+        Console.WriteLine("Parabéns! Você acertou!");
+        Console.WriteLine();
+    }
+    else if (NumPalpite < sortNum)
+    {
+        Console.WriteLine("Tente um número maior!");
+    }
+    else if (NumPalpite > sortNum)
+    {
+        Console.WriteLine("Tente mais baixo!");
+    }
+    Console.ReadLine();
+}
