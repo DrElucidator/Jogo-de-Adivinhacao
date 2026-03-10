4900fdf9d92653406eb82572f2f83171ec7e3eb7
Alec Luí <dr.elucidator@gmail.com>
2026-03-10 15:54:28 -0300

    Adicionado sistema de pontuações, estrutira aprimorada, funcionalidade de múltiplas tentativas e mensagens comunicativas com o usuário aprimoradas

--- JogoDeAdivinhacao.ConsoleApp/Program.cs
+++ JogoDeAdivinhacao.ConsoleApp/Program.cs
@@ -1,33 +1,134 @@
 ﻿using System.Security.Cryptography;
-bool jogarNovamente = true;
 
-Console.WriteLine("---------------------------------");
+Console.Clear();
+
+Console.WriteLine("--------------------------------------------");
 Console.WriteLine("Bem-vindo ao Jogo de Adivinhação!");
-Console.WriteLine("---------------------------------");
+Console.WriteLine("--------------------------------------------");
 
-while (jogarNovamente == true)
+Console.WriteLine();
+Console.WriteLine("Digite ENTER para iniciar...");
+Console.ReadLine();
+
+while (true)
 {
-    int sortNum = RandomNumberGenerator.GetInt32(1, 21);
-    Console.WriteLine("Adivinhe o número entre 1 e 20: ");
-    string? palpite = Console.ReadLine();
+    Console.Clear();
+    int[] numDigitados = new int[100];
+    int contadorDePalpites = 0;
+    int pontuacao = 1000;
+    Console.WriteLine("Escolha o nível de dificuldade: ");
+    Console.WriteLine();
+    Console.WriteLine("1 - Fácil (10 tentativas)");
+    Console.WriteLine("2 - Médio (5 tentativas)");
+    Console.WriteLine("3 - Difícil (3 tentativas)");
+    Console.WriteLine();
+    Console.WriteLine("Digite a sua escolha: ");
+    string? dificuldade = Console.ReadLine();
 
-    int NumPalpite = Convert.ToInt32(palpite);
-
-    Console.WriteLine("O número digitado foi: " + palpite);
-    Console.WriteLine("O número sorteado foi: " + sortNum);
-
-    if (NumPalpite == sortNum)
+    int maxNum;
+    int maxTry;
+    switch (dificuldade)
     {
-        Console.WriteLine("Parabéns! Você acertou!");
+        case "1":
+            maxNum = 20;
+            maxTry = 10;
+            break;
+        case "2":
+            maxNum = 50;
+            maxTry = 5;
+            break;
+        case "3":
+            maxNum = 100;
+            maxTry = 3;
+            break;
+        default:
+            Console.WriteLine("Opção inválida, escolha uma opção válida.");
+            continue;
+    }
+
+    int sortNum = RandomNumberGenerator.GetInt32(1, maxNum + 1);
+
+    for (int tentativa = 1; tentativa <= maxTry; tentativa++)
+    {
+        Console.Clear();
+        Console.WriteLine($"Tentativa {tentativa} de {maxTry}");
         Console.WriteLine();
+        Console.WriteLine($"Digite o número entre 1 e {maxNum}: ");
+        string? palpite = Console.ReadLine();
+
+        if (!int.TryParse(palpite, out int NumPalpite))
+        {
+            Console.WriteLine("Entrada inválida. Digite um número.");
+            tentativa--;
+            Console.ReadLine();
+            continue;
+        }
+
+        bool oldNum = false;
+        for (int i = 0; i < contadorDePalpites; i++)
+        {
+            if (numDigitados[i] == NumPalpite)
+            {
+                oldNum = true;
+                break;
+            }
+        }
+
+        if (oldNum)
+        {
+            Console.WriteLine("Você já digitou esse número, tente outro.");
+            tentativa--;
+            Console.ReadLine();
+            continue;
+        }
+
+        numDigitados[contadorDePalpites++] = NumPalpite;
+
+        Console.WriteLine("O número digitado foi: " + NumPalpite);
+
+        if (NumPalpite == sortNum)
+        {
+            Console.Clear();
+            Console.WriteLine("Parabéns! Você acertou!");
+            Console.WriteLine("O número sorteado foi: " + sortNum);
+            break;
+        }
+        else if (NumPalpite < sortNum)
+        {
+            Console.WriteLine("Tente um número maior!");
+        }
+        else
+        {
+            Console.WriteLine("Tente um número menor!");
+        }
+
+        int DiffNum = Math.Abs(sortNum - NumPalpite);
+
+        if (DiffNum >= 10)
+            pontuacao -= 100;
+        else if (DiffNum >= 5)
+            pontuacao -= 50;
+        else
+            pontuacao -= 20;
+
+        Console.WriteLine("Sua pontuação é: " + pontuacao);
+        Console.ReadLine();
+
+        if (tentativa == maxTry)
+        {
+            Console.WriteLine("Que pena, você perdeu!");
+            Console.WriteLine("O número sorteado foi: " + sortNum);
+        }
     }
-    else if (NumPalpite < sortNum)
+
+    Console.WriteLine();
+    Console.WriteLine("Deseja jogar novamente? [s/n]: ");
+    string? opcaoContinuar = Console.ReadLine();
+    if (opcaoContinuar?.ToUpper() != "S")
     {
-        Console.WriteLine("Tente um número maior!");
+        Console.Clear();
+        Console.WriteLine();
+        Console.WriteLine("Obrigado por jogar! Até a próxima!");
+        break;
     }
-    else if (NumPalpite > sortNum)
-    {
-        Console.WriteLine("Tente mais baixo!");
-    }
-    Console.ReadLine();
 }
